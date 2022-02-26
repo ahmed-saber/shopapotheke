dc17d4e0c9fd7dc466c5d781ff0a4460507c6ae6
LAPTOP-RRCBUSA8\Work <smartic@gmail.com>
2022-02-26 11:05:21 +0100

    Adding axios

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+	semi: true,
+	useTabs: true,
+	tabWidth: 4,
+	printWidth: 120,
+	singleQuote: true,
+	arrowParens: 'avoid',
+	trailingComma: 'none'
+};
