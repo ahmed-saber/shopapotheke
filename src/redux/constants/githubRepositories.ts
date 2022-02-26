4654a0c0574c51f4733212d484cfe864cdac4515
LAPTOP-RRCBUSA8\Work <smartic@gmail.com>
2022-02-26 15:12:06 +0100

    Add new constants to get github repos

---
+++ src/redux/constants/githubRepositories.ts
@@ -0,0 +1,3 @@
+export const GET_GITHUB_REPOSITORIES_ERROR = 'get/github/repositories/error';
+export const GET_GITHUB_REPOSITORIES_PENDING = 'get/github/repositories/pending';
+export const GET_GITHUB_REPOSITORIES_SUCCESS = 'get/github/repositories/success';
